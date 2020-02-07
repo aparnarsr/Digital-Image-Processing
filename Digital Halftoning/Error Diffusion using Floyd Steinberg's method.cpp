309ab23e1093afba003fedae81656dce5a515030
Aparna Srikanth <55259892+aparnarsr@users.noreply.github.com>
2020-02-07 10:45:08 -0800

    Add files via upload

---
+++ Digital Halftoning/Error Diffusion using Floyd Steinberg's method.cpp
@@ -0,0 +1,101 @@
+// This sample code reads in image data from a RAW image file and// writes it into another file// NOTE:	The code assumes that the image is of size 256 x 256 and is in the//			RAW format. You will need to make corresponding changes to//			accommodate images of different sizes and/or types#include <stdio.h>#include <iostream>#include <stdlib.h>using namespace std;int main(int argc, char *argv[]){	// Define file pointer and variables	FILE *file;	int BytesPerPixel;	int Size1 = 400;
+	int Size2 = 600;	// Check for proper syntax	if (argc < 3){		cout << "Syntax Error - Incorrect Parameter Usage:" << endl;		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [Size = 256]" << endl;		return 0;	}	// Check if image is grayscale or color	if (argc < 4){		BytesPerPixel = 1; // default is grey image	}	else {		BytesPerPixel = atoi(argv[3]);		// Check if size is specified		if (argc >= 5){			Size1 = atoi(argv[4]);
+			Size2 = atoi(argv[5]);		}	}	// Allocate image data array	unsigned char Imagedatainput[Size1][Size2];	// Read image (filename specified by first argument) into image data matrix	if (!(file=fopen(argv[1],"rb"))) {		cout << "Cannot open file: " << argv[1] <<endl;		exit(1);	}	fread(Imagedatainput, sizeof(unsigned char), Size1*Size2, file);	fclose(file);	///////////////////////// INSERT YOUR PROCESSING CODE HERE /////////////////////////
+
+//Assigning a boundary extended matrix
+unsigned char Imagedata[Size1+2][Size2+2];
+
+//Boundary extension for the columns
+
+for(int i= 0; i<(Size1+1); i++){
+    Imagedata[i][0]=(unsigned char)0;
+    Imagedata[i][Size2+1]=(unsigned char)0;
+
+}
+
+//Boundary extension for the rows
+        for(int i= 1; i<(Size2+1); i++){
+            Imagedata[0][i]=(unsigned char)0;
+            Imagedata[Size1+1][i]=(unsigned char)0;
+
+        }
+
+//Values taken for Imagedata from Imagedatainput
+    for(int i=1; i<(Size1+1);i++){
+        for(int j=1; j<(Size2+1); j++){
+            Imagedata[i][j]=Imagedatainput[i-1][j-1];
+        }
+    }
+//Corner matrices formed for Imagedata
+
+Imagedata[0][0]=(unsigned char)0;
+Imagedata[0][Size2+1]=(unsigned char)0;
+Imagedata[Size1+1][0]=(unsigned char)0;
+Imagedata[Size1+1][Size2+1]=(unsigned char)0;
+
+
+unsigned char Imagedatadif[Size1+2][Size2+2];
+unsigned char Imagedataoutput[Size1][Size2];
+
+
+for(int i=0;i<Size1+2;i++){
+    for(int j=0;j<Size2+2;j++){
+        Imagedatadif[i][j]=Imagedata[i][j];
+    }
+}
+
+for(int i=1; i<Size1+1; i+=2){
+    for(int j=1; j<Size2+1; j++){
+        int pixel=Imagedatadif[i][j];
+        int temp;
+        if(pixel>=127){
+            temp=255;
+        }
+        else{
+            temp=0;
+        }
+        int e=Imagedatadif[i][j]-temp;
+
+        Imagedatadif[i][j+1]=Imagedatadif[i][j+1]+((e*7)/16);
+        Imagedatadif[i+1][j+1]=Imagedatadif[i+1][j+1]+((e*1)/16);
+        Imagedatadif[i+1][j]=Imagedatadif[i+1][j]+((e*5)/16);
+        Imagedatadif[i+1][j-1]=Imagedatadif[i+1][j-1]+((e*3)/16);
+    }
+        int p=i+1;
+        for(int q=Size2;q>0;q--){
+            int pixel=Imagedatadif[p][q];
+            int temp;
+            if(pixel>=127){
+                temp=255;
+
+            }
+            else{
+                temp=0;
+            }
+            int e=Imagedatadif[p][q]-temp;
+            Imagedatadif[p][q-1]=Imagedatadif[p][q-1]+((e*7)/16);
+            Imagedatadif[p+1][q-1]=Imagedatadif[p+1][q-1]+((e*1)/16);
+            Imagedatadif[p+1][q]=Imagedatadif[p+1][q]+((e*5)/16);
+            Imagedatadif[p+1][q+1]=Imagedatadif[p+1][q+1]+((e*3)/16);
+
+        }
+
+}
+
+
+for(int i=0;i<Size1;i++){
+    for(int j=0;j<Size2;j++){
+
+        Imagedataoutput[i][j]=Imagedatadif[i+1][j+1];
+
+    }
+}
+for(int i=0; i<Size1;i++){
+    for(int j=0; j<Size2; j++){
+        if(Imagedataoutput[i][j]>=127){
+            Imagedataoutput[i][j]=255;
+        }
+        else
+            Imagedataoutput[i][j]=0;
+    }
+}	// Write image data (filename specified by second argument) from image data matrix	if (!(file=fopen(argv[2],"wb"))) {		cout << "Cannot open file: " << argv[2] << endl;		exit(1);	}	fwrite(Imagedataoutput, sizeof(unsigned char), Size1*Size2, file);	fclose(file);	return 0;}
