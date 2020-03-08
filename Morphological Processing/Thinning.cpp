3410ec9baa942096db93f34ba704a55dd637b827
Aparna Srikanth <55259892+aparnarsr@users.noreply.github.com>
2020-03-08 12:32:34 -0700

    Rename DIPHW3P2a2.cpp to Thinning.cpp

--- Morphological Processing/Thinning.cpp
+++ Morphological Processing/Thinning.cpp
@@ -1,6 +1,57 @@
-// This sample code reads in image data from a RAW image file and// writes it into another file// NOTE:	The code assumes that the image is of size 256 x 256 and is in the//			RAW format. You will need to make corresponding changes to//			accommodate images of different sizes and/or types#include <stdio.h>#include <iostream>#include <stdlib.h>
+// This sample code reads in image data from a RAW image file and
+// writes it into another file
+
+// NOTE:	The code assumes that the image is of size 256 x 256 and is in the
+//			RAW format. You will need to make corresponding changes to
+//			accommodate images of different sizes and/or types
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
 #include <math.h>
-#include <vector>using namespace std;int main(int argc, char *argv[]){	// Define file pointer and variables	FILE *file;	int BytesPerPixel;	int Size = 375;	// Check for proper syntax	if (argc < 3){		cout << "Syntax Error - Incorrect Parameter Usage:" << endl;		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [Size = 256]" << endl;		return 0;	}	// Check if image is grayscale or color	if (argc < 4){		BytesPerPixel = 1; // default is grey image	}	else {		BytesPerPixel = atoi(argv[3]);		// Check if size is specified		if (argc >= 5){			Size = atoi(argv[4]);		}	}	// Allocate image data array	unsigned char Imagedatainput[Size][Size];	// Read image (filename specified by first argument) into image data matrix	if (!(file=fopen(argv[1],"rb"))) {		cout << "Cannot open file: " << argv[1] <<endl;		exit(1);	}	fread(Imagedatainput, sizeof(unsigned char), Size*Size, file);	fclose(file);	///////////////////////// INSERT YOUR PROCESSING CODE HERE /////////////////////////
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+
+{
+	// Define file pointer and variables
+	FILE *file;
+	int BytesPerPixel;
+	int Size = 375;
+
+	// Check for proper syntax
+	if (argc < 3){
+		cout << "Syntax Error - Incorrect Parameter Usage:" << endl;
+		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [Size = 256]" << endl;
+		return 0;
+	}
+
+	// Check if image is grayscale or color
+	if (argc < 4){
+		BytesPerPixel = 1; // default is grey image
+	}
+	else {
+		BytesPerPixel = atoi(argv[3]);
+		// Check if size is specified
+		if (argc >= 5){
+			Size = atoi(argv[4]);
+		}
+	}
+
+	// Allocate image data array
+	unsigned char Imagedatainput[Size][Size];
+
+	// Read image (filename specified by first argument) into image data matrix
+	if (!(file=fopen(argv[1],"rb"))) {
+		cout << "Cannot open file: " << argv[1] <<endl;
+		exit(1);
+	}
+	fread(Imagedatainput, sizeof(unsigned char), Size*Size, file);
+	fclose(file);
+
+	///////////////////////// INSERT YOUR PROCESSING CODE HERE /////////////////////////
 unsigned char M[Size+2][Size+2];
 unsigned char Imagedatainputext[Size+2][Size+2];
 unsigned char Imagedataout[Size][Size];
@@ -985,7 +1036,19 @@ for(int i=0;i<Size;i++){
 }
 cout<<"end";
 cout<<"otter"<<otter<<" "<<"hit"<<hit;
-	// Write image data (filename specified by second argument) from image data matrix	if (!(file=fopen(argv[2],"wb"))) {		cout << "Cannot open file: " << argv[2] << endl;		exit(1);	}	fwrite(Imagedataout, sizeof(unsigned char), (Size)*(Size)*BytesPerPixel, file);	fclose(file);	return 0;}
+
+	// Write image data (filename specified by second argument) from image data matrix
+
+	if (!(file=fopen(argv[2],"wb"))) {
+		cout << "Cannot open file: " << argv[2] << endl;
+		exit(1);
+	}
+	fwrite(Imagedataout, sizeof(unsigned char), (Size)*(Size)*BytesPerPixel, file);
+	fclose(file);
+
+	return 0;
+}
+
 
 
 
